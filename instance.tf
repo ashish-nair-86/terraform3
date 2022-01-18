65ce9655ea2e0b6063f5c9ec1d75cd3e2ff3465b
jenkins <jenkins@docker.example.com>
2022-01-18 09:21:56 +0530

    revert changes

--- instance.tf
+++ instance.tf
@@ -5,14 +5,14 @@ resource "aws_key_pair" "dove-key" {
 }
 
 resource "aws_instance" "dover-instance" {
-  count                  = ${count}
+  count                  = 2
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
   key_name               = aws_key_pair.dove-key.key_name
   vpc_security_group_ids = ["sg-00a7fdf11efbd18ad"]
   tags = {
-    Name = "${name}-${count.index + 1}"
+    Name = "App-${count.index + 1}"
   }
   provisioner "file" {
     source      = "web.sh"
